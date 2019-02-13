dfac25965009ac89001fb6f92a02d7e1d03393e3
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 18:24:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,18 +12,17 @@ def consolidate_cart(cart)
   new_cart
 end
 
-def apply_coupons(cart, coupons)
+def apply_coupons(cart: [], coupons: [])
   couponed_items= {}
   coupons.each do |coupon_hash|
     cart.each do |name, item_hash|
       if name == coupon_hash[:item] && item_hash[:count] >= coupon_hash[:num]
         item_hash[:count] = item_hash[:count] - coupon_hash[:num]
-
         new_name = coupon_hash[:item] + " w/coupon"
-        couponed_items[new_name] = { :price => coupon_hash[:cost],
-                                     :count => 1,
+        couponed_items[new_name] = { :price => coupon_hash[:cost], 
+                                     :count => 1, 
                                      :clearance => item_hash[:clearance] }
-      end
+      end 
     end
   end
   cart.merge(couponed_items)
