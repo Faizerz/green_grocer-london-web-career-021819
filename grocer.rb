d2692d924809896a47b21950e3d41eaa7b2e419a
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 17:43:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,26 +1,15 @@
 def consolidate_cart(cart)
   new_cart = {}
   cart.each do |cart_hash|
-    cart_hash.each_with_oj do |veg, data|
-      new_cart[veg] ||= {}
-      new_cart[veg][:count] += 1
-      new_cart[veg] << data
-    end
-  end  
-end
-
-
-def consolidate_cart(cart)
-  cart.each_with_object({}) do |item, result|
-    item.each do |type, attributes|
-      if result[type]
-        attributes[:count] += 1
+    cart_hash.each do |veg, data|
+      if new_cart[veg]
+        new_cart[veg][:count] += 1
       else
-        attributes[:count] = 1
-        result[type] = attributes
-      end
+        new_cart[veg] << data
+        new_cart[veg][:count] = 1
     end
   end
+  new_cart
 end
 
 end
