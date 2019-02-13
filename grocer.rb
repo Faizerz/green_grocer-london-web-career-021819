905c13aafd611edda8aad7900a344f923c0de2cb
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 17:46:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,16 +12,16 @@ def consolidate_cart(cart)
   new_cart
 end
 
-end
+# end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+# def apply_coupons(cart, coupons)
+#   # code here
+# end
 
-def apply_clearance(cart)
-  # code here
-end
+# def apply_clearance(cart)
+#   # code here
+# end
 
-def checkout(cart, coupons)
-  # code here
-end
+# def checkout(cart, coupons)
+#   # code here
+# end
